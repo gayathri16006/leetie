7b414d8457ef0a3547d254425f03c9d9fb804773
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-20 13:35:51 +0000

    leetie: Add 289. Game of Life [Medium] (python)

--- solutions/Medium/0289-game-of-life/solution.py
+++ solutions/Medium/0289-game-of-life/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, Matrix, Simulation
 # Link     : https://leetcode.com/problems/game-of-life/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12492000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 12412000 (beats 21%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
