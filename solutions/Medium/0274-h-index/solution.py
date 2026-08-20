b7c22180a9bd37e8a87e9523802e1920ec824a0a
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-20 13:31:53 +0000

    leetie: Add 274. H-Index [Medium] (python)

--- solutions/Medium/0274-h-index/solution.py
+++ solutions/Medium/0274-h-index/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, Sorting, Counting Sort
 # Link     : https://leetcode.com/problems/h-index/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12432000 (beats 0%)
+# Runtime  : 4 ms (beats 21%)
+# Memory   : 12504000 (beats 77%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
