74f6227872cfcd82dcf1f31963f487590f2abab2
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-20 13:33:45 +0000

    leetie: Add 278. First Bad Version [Easy] (python)

--- solutions/Easy/0278-first-bad-version/solution.py
+++ solutions/Easy/0278-first-bad-version/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Easy
 # Tags     : Binary Search, Interactive
 # Link     : https://leetcode.com/problems/first-bad-version/
-# Runtime  : 15 ms (beats 0%)
-# Memory   : 12140000 (beats 0%)
+# Runtime  : 8 ms (beats 92%)
+# Memory   : 12244000 (beats 88%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
