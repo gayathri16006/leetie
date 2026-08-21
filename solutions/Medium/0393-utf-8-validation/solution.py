2abac08cc5e76131b1a66f6b2fb6b67799d1f7a7
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-21 08:36:01 +0000

    leetie: Add 393. UTF-8 Validation [Medium] (python)

--- solutions/Medium/0393-utf-8-validation/solution.py
+++ solutions/Medium/0393-utf-8-validation/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, Bit Manipulation
 # Link     : https://leetcode.com/problems/utf-8-validation/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12288000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 12636000 (beats 38%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
