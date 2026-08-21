08f120c1a1d032109ecaa0f8ced15363e00b552a
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-21 09:43:03 +0000

    leetie: Add 401. Binary Watch [Easy] (python)

--- solutions/Easy/0401-binary-watch/solution.py
+++ solutions/Easy/0401-binary-watch/solution.py
@@ -4,16 +4,27 @@
 # Tags     : Backtracking, Bit Manipulation
 # Link     : https://leetcode.com/problems/binary-watch/
 # Runtime  : 0 ms (beats 0%)
-# Memory   : 12320000 (beats 0%)
+# Memory   : 12556000 (beats 0%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
 # ──────────────────────────────────────────────────
 
 class Solution(object):
+
     def readBinaryWatch(self, turnedOn):
         """
         :type turnedOn: int
         :rtype: List[str]
         """
-        
+        res = []
+
+        # Iterate through all valid hours and minutes
+        for h in range(12):
+            for m in range(60):
+                # Count total set bits for the hour and minute combination
+                if (bin(h).count("1") + bin(m).count("1")) == turnedOn:
+                    # Minutes must be formatted with 2 digits (e.g. 05)
+                    res.append("{:d}:{:02d}".format(h, m))
+
+        return res
