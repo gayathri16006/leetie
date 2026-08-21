31a3356d7dbbe8f7c8a6e4b6cd79a28e996fb460
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-21 08:35:06 +0000

    leetie: Add 390. Elimination Game [Medium] (python)

--- solutions/Medium/0390-elimination-game/solution.py
+++ solutions/Medium/0390-elimination-game/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Math, Recursion
 # Link     : https://leetcode.com/problems/elimination-game/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12528000 (beats 0%)
+# Runtime  : 4 ms (beats 69%)
+# Memory   : 12296000 (beats 91%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
