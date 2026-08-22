9511028fbf09be7d0f67b4ba58a7f7a05b9ee497
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-22 12:20:43 +0000

    leetie: Add 468. Validate IP Address [Medium] (python)

--- solutions/Medium/0468-validate-ip-address/solution.py
+++ solutions/Medium/0468-validate-ip-address/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : String
 # Link     : https://leetcode.com/problems/validate-ip-address/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12540000 (beats 0%)
+# Runtime  : 2 ms (beats 8%)
+# Memory   : 12288000 (beats 95%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
