962e43c7ac4d618f6d8f868d4a1f4f5cb3ce03af
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-22 17:00:47 +0000

    leetie: Add 502. IPO [Hard] (python)

---
+++ solutions/Hard/0502-ipo/solution.py
@@ -0,0 +1,22 @@
+# ──────────────────────────────────────────────────
+# Problem  : 502. IPO
+# Difficulty: Hard
+# Tags     : Array, Greedy, Sorting, Heap (Priority Queue)
+# Link     : https://leetcode.com/problems/ipo/
+# Runtime  : 0 ms (beats 0%)
+# Memory   : 12224000 (beats 0%)
+# Language : python
+# Copyright: (c) 2026 gayathri16006. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+class Solution(object):
+    def findMaximizedCapital(self, k, w, profits, capital):
+        """
+        :type k: int
+        :type w: int
+        :type profits: List[int]
+        :type capital: List[int]
+        :rtype: int
+        """
+        
