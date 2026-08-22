f1e382472ab16c1d217967e04784085dffc3c4ce
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-22 16:58:50 +0000

    leetie: Add 500. Keyboard Row [Easy] (python)

---
+++ solutions/Easy/0500-keyboard-row/solution.py
@@ -0,0 +1,29 @@
+# ──────────────────────────────────────────────────
+# Problem  : 500. Keyboard Row
+# Difficulty: Easy
+# Tags     : Array, Hash Table, String
+# Link     : https://leetcode.com/problems/keyboard-row/
+# Runtime  : 0 ms (beats 0%)
+# Memory   : 12208000 (beats 0%)
+# Language : python
+# Copyright: (c) 2026 gayathri16006. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        row1 = set("qwertyuiop")
+        row2 = set("asdfghjkl")
+        row3 = set("zxcvbnm")
+
+        valid_words = []
+        for word in words:
+            word_chars = set(word.lower())
+            if word_chars <= row1 or word_chars <= row2 or word_chars <= row3:
+                valid_words.append(word)
+
+        return valid_words
