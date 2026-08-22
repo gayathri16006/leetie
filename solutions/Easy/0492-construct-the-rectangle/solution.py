f788733f17bbe1eb20567981976f322d3e7ad65b
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-22 16:20:58 +0000

    leetie: Add 492. Construct the Rectangle [Easy] (python)

--- solutions/Easy/0492-construct-the-rectangle/solution.py
+++ solutions/Easy/0492-construct-the-rectangle/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Easy
 # Tags     : Math
 # Link     : https://leetcode.com/problems/construct-the-rectangle/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12364000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 12272000 (beats 91%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
