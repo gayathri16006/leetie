eff40ed650cfaab036f4207526304f8b04f62bb7
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-22 12:09:27 +0000

    leetie: Add 455. Assign Cookies [Easy] (python)

--- solutions/Easy/0455-assign-cookies/solution.py
+++ solutions/Easy/0455-assign-cookies/solution.py
@@ -4,7 +4,7 @@
 # Tags     : Array, Two Pointers, Greedy, Sorting, Quicksort
 # Link     : https://leetcode.com/problems/assign-cookies/
 # Runtime  : 0 ms (beats 0%)
-# Memory   : 12208000 (beats 0%)
+# Memory   : 12228000 (beats 0%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
@@ -17,4 +17,16 @@ class Solution(object):
         :type s: List[int]
         :rtype: int
         """
+        g.sort()
+        s.sort()
         
+        child_i = 0
+        cookie_j = 0
+        
+        # Try to satisfy the least greedy child with the smallest possible cookie
+        while child_i < len(g) and cookie_j < len(s):
+            if s[cookie_j] >= g[child_i]:
+                child_i += 1
+            cookie_j += 1
+            
+        return child_i
