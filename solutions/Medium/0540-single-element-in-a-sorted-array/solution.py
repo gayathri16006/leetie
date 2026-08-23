7a200dd403746c446d7fa7bc2d78164bb890be2a
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-23 03:33:14 +0000

    leetie: Add 540. Single Element in a Sorted Array [Medium] (python)

--- solutions/Medium/0540-single-element-in-a-sorted-array/solution.py
+++ solutions/Medium/0540-single-element-in-a-sorted-array/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, Binary Search
 # Link     : https://leetcode.com/problems/single-element-in-a-sorted-array/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12576000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 16884000 (beats 23%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
