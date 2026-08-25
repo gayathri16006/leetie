3da236e3d8cad972973e910a226ba98adffe6349
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-25 07:55:04 +0000

    leetie: Add 682. Baseball Game [Easy] (python3)

--- solutions/Easy/0682-baseball-game/solution.py
+++ solutions/Easy/0682-baseball-game/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Easy
 # Tags     : Array, Stack, Simulation
 # Link     : https://leetcode.com/problems/baseball-game/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 19224000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 19348000 (beats 81%)
 # Language : python3
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
