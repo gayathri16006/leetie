e4947669da1e383cc5cc25b319f51333dad0c58a
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-25 07:17:46 +0000

    leetie: Add 665. Non-decreasing Array [Medium] (python3)

--- solutions/Medium/0665-non-decreasing-array/solution.py
+++ solutions/Medium/0665-non-decreasing-array/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array
 # Link     : https://leetcode.com/problems/non-decreasing-array/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 19460000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 20520000 (beats 45%)
 # Language : python3
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
