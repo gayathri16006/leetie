cd89daf6e0ed208a547f34ae23e2363d1ecb39e2
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-08-25 08:34:05 +0000

    leetie: Add 709. To Lower Case [Easy] (python3)

---
+++ solutions/Easy/0709-to-lower-case/solution.py
@@ -0,0 +1,16 @@
+# ──────────────────────────────────────────────────
+# Problem  : 709. To Lower Case
+# Difficulty: Easy
+# Tags     : String
+# Link     : https://leetcode.com/problems/to-lower-case/
+# Runtime  : 0 ms (beats 0%)
+# Memory   : 19320000 (beats 0%)
+# Language : python3
+# Copyright: (c) 2026 gayathri16006. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        # Built-in method
+        return s.lower()
