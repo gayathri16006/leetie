43e13515727aae964fbfbf7116bb19e2d29bb82b
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-03 17:17:07 +0000

    leetie: Add 806. Number of Lines To Write String [Easy] (python)

--- solutions/Easy/0806-number-of-lines-to-write-string/solution.py
+++ solutions/Easy/0806-number-of-lines-to-write-string/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Easy
 # Tags     : Array, String
 # Link     : https://leetcode.com/problems/number-of-lines-to-write-string/
-# Runtime  : 12 ms (beats 0%)
-# Memory   : 12504000 (beats 0%)
+# Runtime  : 27 ms (beats 7%)
+# Memory   : 12408000 (beats 31%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
