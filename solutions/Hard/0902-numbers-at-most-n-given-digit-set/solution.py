ee93651e044a2b4c32a8704c063cb2e3d54b393d
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-04 11:41:03 +0000

    leetie: Add 902. Numbers At Most N Given Digit Set [Hard] (python)

--- solutions/Hard/0902-numbers-at-most-n-given-digit-set/solution.py
+++ solutions/Hard/0902-numbers-at-most-n-given-digit-set/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Hard
 # Tags     : Array, Math, String, Binary Search, Dynamic Programming
 # Link     : https://leetcode.com/problems/numbers-at-most-n-given-digit-set/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12572000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 12456000 (beats 20%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
