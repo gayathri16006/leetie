d6c9088aa921b6a333439525c29a4125e8cce618
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-04 10:52:00 +0000

    leetie: Add 812. Largest Triangle Area [Easy] (python)

---
+++ solutions/Easy/0812-largest-triangle-area/solution.py
@@ -0,0 +1,28 @@
+# ──────────────────────────────────────────────────
+# Problem  : 812. Largest Triangle Area
+# Difficulty: Easy
+# Tags     : Array, Math, Geometry, Polygons
+# Link     : https://leetcode.com/problems/largest-triangle-area/
+# Runtime  : 0 ms (beats 0%)
+# Memory   : 12336000 (beats 0%)
+# Language : python
+# Copyright: (c) 2026 gayathri16006. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+import itertools
+
+class Solution(object):
+    def largestTriangleArea(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: float
+        """
+        def area(p1, p2, p3):
+            return 0.5 * abs(
+                p1[0] * (p2[1] - p3[1]) +
+                p2[0] * (p3[1] - p1[1]) +
+                p3[0] * (p1[1] - p2[1])
+            )
+
+        return max(area(p1, p2, p3) for p1, p2, p3 in itertools.combinations(points, 3))
