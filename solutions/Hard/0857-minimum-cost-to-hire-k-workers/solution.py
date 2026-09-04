2fac21be7c065507537aba669b349bb83d440a85
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-04 11:11:20 +0000

    leetie: Add 857. Minimum Cost to Hire K Workers [Hard] (python)

--- solutions/Hard/0857-minimum-cost-to-hire-k-workers/solution.py
+++ solutions/Hard/0857-minimum-cost-to-hire-k-workers/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Hard
 # Tags     : Array, Greedy, Sorting, Heap (Priority Queue)
 # Link     : https://leetcode.com/problems/minimum-cost-to-hire-k-workers/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12328000 (beats 0%)
+# Runtime  : 121 ms (beats 15%)
+# Memory   : 15368000 (beats 18%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
