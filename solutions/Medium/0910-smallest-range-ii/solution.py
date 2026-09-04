184a767e390a090fa5d4aa829c116ea1b4eb50e1
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-04 11:47:56 +0000

    leetie: Add 910. Smallest Range II [Medium] (python)

--- solutions/Medium/0910-smallest-range-ii/solution.py
+++ solutions/Medium/0910-smallest-range-ii/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, Math, Greedy, Sorting
 # Link     : https://leetcode.com/problems/smallest-range-ii/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12420000 (beats 0%)
+# Runtime  : 35 ms (beats 75%)
+# Memory   : 13360000 (beats 5%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
