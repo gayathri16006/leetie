1e2804b73c000516e54b5bc9345fbde545d279a9
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-18 03:58:21 +0000

    leetie: Add 955. Delete Columns to Make Sorted II [Medium] (python)

--- solutions/Medium/0955-delete-columns-to-make-sorted-ii/solution.py
+++ solutions/Medium/0955-delete-columns-to-make-sorted-ii/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, String, Greedy
 # Link     : https://leetcode.com/problems/delete-columns-to-make-sorted-ii/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12400000 (beats 0%)
+# Runtime  : 3 ms (beats 92%)
+# Memory   : 12540000 (beats 6%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
