3be75f0a64d4e5fb70cc2038322dd9506302b23a
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-18 03:59:32 +0000

    leetie: Add 958. Check Completeness of a Binary Tree [Medium] (python)

---
+++ solutions/Medium/0958-check-completeness-of-a-binary-tree/solution.py
@@ -0,0 +1,41 @@
+# ──────────────────────────────────────────────────
+# Problem  : 958. Check Completeness of a Binary Tree
+# Difficulty: Medium
+# Tags     : Tree, Breadth-First Search, Binary Tree
+# Link     : https://leetcode.com/problems/check-completeness-of-a-binary-tree/
+# Runtime  : 0 ms (beats 0%)
+# Memory   : 12300000 (beats 0%)
+# Language : python
+# Copyright: (c) 2026 gayathri16006. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isCompleteTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        queue = deque([root])
+        seen_null = False
+
+        while queue:
+            node = queue.popleft()
+
+            if not node:
+                seen_null = True
+            else:
+                if seen_null:
+                    return False
+                queue.append(node.left)
+                queue.append(node.right)
+
+        return True
