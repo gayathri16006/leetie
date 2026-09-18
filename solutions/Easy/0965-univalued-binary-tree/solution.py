26d682656df1d067f33a42b187a89bfbf3ea182a
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-18 04:02:21 +0000

    leetie: Add 965. Univalued Binary Tree [Easy] (python)

--- solutions/Easy/0965-univalued-binary-tree/solution.py
+++ solutions/Easy/0965-univalued-binary-tree/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Easy
 # Tags     : Tree, Depth-First Search, Breadth-First Search, Binary Tree
 # Link     : https://leetcode.com/problems/univalued-binary-tree/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12524000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 12424000 (beats 52%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
@@ -30,7 +30,7 @@ class Solution(object):
             if root.left.val != root.val or not self.isUnivalTree(root.left):
                 return False
                 
-        # Check right child value
+        
         if root.right:
             if root.right.val != root.val or not self.isUnivalTree(root.right):
                 return False
