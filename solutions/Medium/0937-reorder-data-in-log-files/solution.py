453c74c0a5b590fbc96c787e7cb55520474d7961
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-18 03:22:03 +0000

    leetie: Add 937. Reorder Data in Log Files [Medium] (python)

--- solutions/Medium/0937-reorder-data-in-log-files/solution.py
+++ solutions/Medium/0937-reorder-data-in-log-files/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, String, Sorting
 # Link     : https://leetcode.com/problems/reorder-data-in-log-files/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12456000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 12500000 (beats 58%)
 # Language : python
 # Copyright: (c) 2026 gayathri16006. All rights reserved.
 # Synced by: leetie
