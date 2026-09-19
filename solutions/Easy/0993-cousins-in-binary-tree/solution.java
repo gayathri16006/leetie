ffe1eb53aaa6145ffb1a642bdf0958fa1a376b6f
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-19 13:26:04 +0000

    leetie: Add 993. Cousins in Binary Tree [Easy] (java)

--- solutions/Easy/0993-cousins-in-binary-tree/solution.java
+++ solutions/Easy/0993-cousins-in-binary-tree/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Tree, Depth-First Search, Breadth-First Search, Binary Tree
 // Link     : https://leetcode.com/problems/cousins-in-binary-tree/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42180000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 43468000 (beats 8%)
 // Language : java
 // Copyright: (c) 2026 gayathri16006. All rights reserved.
 // Synced by: leetie
