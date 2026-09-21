94de7ea7b9d4278a8ddc9f5edcc63be95466a640
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-21 17:02:57 +0000

    leetie: Add 1009. Complement of Base 10 Integer [Easy] (java)

--- solutions/Easy/1009-complement-of-base-10-integer/solution.java
+++ solutions/Easy/1009-complement-of-base-10-integer/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Bit Manipulation
 // Link     : https://leetcode.com/problems/complement-of-base-10-integer/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42008000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 41944000 (beats 84%)
 // Language : java
 // Copyright: (c) 2026 gayathri16006. All rights reserved.
 // Synced by: leetie
