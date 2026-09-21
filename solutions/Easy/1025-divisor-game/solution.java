dbba1cff5a282080577975b8bb9e23a3e200683b
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-21 13:54:51 +0000

    leetie: Add 1025. Divisor Game [Easy] (java)

--- solutions/Easy/1025-divisor-game/solution.java
+++ solutions/Easy/1025-divisor-game/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, Dynamic Programming, Brainteaser, Game Theory, Impartial Game
 // Link     : https://leetcode.com/problems/divisor-game/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42352000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 42296000 (beats 16%)
 // Language : java
 // Copyright: (c) 2026 gayathri16006. All rights reserved.
 // Synced by: leetie
