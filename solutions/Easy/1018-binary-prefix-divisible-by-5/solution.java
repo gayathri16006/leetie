f789c7fc623be4faea586d65307a4d594d3671bc
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-21 17:24:48 +0000

    leetie: Add 1018. Binary Prefix Divisible By 5 [Easy] (java)

--- solutions/Easy/1018-binary-prefix-divisible-by-5/solution.java
+++ solutions/Easy/1018-binary-prefix-divisible-by-5/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Bit Manipulation
 // Link     : https://leetcode.com/problems/binary-prefix-divisible-by-5/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42596000 (beats 0%)
+// Runtime  : 3 ms (beats 99%)
+// Memory   : 47364000 (beats 92%)
 // Language : java
 // Copyright: (c) 2026 gayathri16006. All rights reserved.
 // Synced by: leetie
