60018a2ff722a35c20d11bf82249e89efc2b9884
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-22 09:11:14 +0000

    leetie: Add 1139. Largest 1-Bordered Square [Medium] (java)

--- solutions/Medium/1139-largest-1-bordered-square/solution.java
+++ solutions/Medium/1139-largest-1-bordered-square/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Dynamic Programming, Matrix
 // Link     : https://leetcode.com/problems/largest-1-bordered-square/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42552000 (beats 0%)
+// Runtime  : 4 ms (beats 100%)
+// Memory   : 46916000 (beats 39%)
 // Language : java
 // Copyright: (c) 2026 gayathri16006. All rights reserved.
 // Synced by: leetie
@@ -15,8 +15,7 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        // left[i][j]: consecutive 1s ending at (i, j) horizontally
-        // top[i][j]:  consecutive 1s ending at (i, j) vertically
+        
         int[][] left = new int[m][n];
         int[][] top = new int[m][n];
 
@@ -31,13 +30,13 @@ class Solution {
 
         int maxSide = 0;
 
-        // Consider each cell (i, j) as the bottom-right corner of the square
+       
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 int maxPossible = Math.min(left[i][j], top[i][j]);
 
                 for (int k = maxPossible; k > maxSide; k--) {
-                    // Check if top edge and left edge also have length >= k
+                    
                     if (left[i - k + 1][j] >= k && top[i][j - k + 1] >= k) {
                         maxSide = k;
                         break;
