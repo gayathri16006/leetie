0b0ca0e443f8e733cd05f6b0f78f913fb62d5154
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-22 09:15:39 +0000

    leetie: Add 1160. Find Words That Can Be Formed by Characters [Easy] (java)

--- solutions/Easy/1160-find-words-that-can-be-formed-by-characters/solution.java
+++ solutions/Easy/1160-find-words-that-can-be-formed-by-characters/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Hash Table, String, Counting
 // Link     : https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42836000 (beats 0%)
+// Runtime  : 6 ms (beats 89%)
+// Memory   : 47332000 (beats 85%)
 // Language : java
 // Copyright: (c) 2026 gayathri16006. All rights reserved.
 // Synced by: leetie
