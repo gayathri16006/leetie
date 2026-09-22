3f63355a105d66e2440b003c1200990d7e7ff1c0
gayathri16006 <gayathri16006@users.noreply.github.com>
2026-09-22 09:12:23 +0000

    leetie: Add 1143. Longest Common Subsequence [Medium] (java)

--- solutions/Medium/1143-longest-common-subsequence/solution.java
+++ solutions/Medium/1143-longest-common-subsequence/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : String, Dynamic Programming, Longest Common Subsequence
 // Link     : https://leetcode.com/problems/longest-common-subsequence/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 43148000 (beats 0%)
+// Runtime  : 11 ms (beats 98%)
+// Memory   : 42988000 (beats 96%)
 // Language : java
 // Copyright: (c) 2026 gayathri16006. All rights reserved.
 // Synced by: leetie
@@ -14,8 +14,8 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
+        
 
-        // Ensure text2 is the shorter string to optimize space to O(min(m, n))
         if (m < n) {
             return longestCommonSubsequence(text2, text1);
         }
@@ -23,10 +23,10 @@ class Solution {
         int[] dp = new int[n + 1];
 
         for (int i = 1; i <= m; i++) {
-            int prev = 0; // Represents dp[i-1][j-1]
+            int prev = 0; 
             char c1 = text1.charAt(i - 1);
             for (int j = 1; j <= n; j++) {
-                int temp = dp[j]; // Store current dp[j] (which is dp[i-1][j] for next column)
+                int temp = dp[j]; 
                 if (c1 == text2.charAt(j - 1)) {
                     dp[j] = prev + 1;
                 } else {
